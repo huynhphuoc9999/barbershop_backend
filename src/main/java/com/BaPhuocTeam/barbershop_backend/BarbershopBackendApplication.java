a1c2282fe4aa62752bce25df6e5ae389095dd2b9
phuoc9999 <phuoc14022001@gmail.com>
2026-03-30 09:11:49 +0700

    Update file main to deploy

--- src/main/java/com/BaPhuocTeam/barbershop_backend/BarbershopBackendApplication.java
+++ src/main/java/com/BaPhuocTeam/barbershop_backend/BarbershopBackendApplication.java
@@ -50,36 +50,36 @@ public class BarbershopBackendApplication {
 		
 
 		//------------ Run Render Deploy -------------------//
-		System.setProperty("DATABASE_URL", System.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USERNAME", System.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", System.get("DATABASE_PASSWORD"));
-		System.setProperty("SERVER.PORT", System.get("SERVER.PORT"));
+		System.setProperty("DATABASE_URL", System.getenv("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", System.getenv("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", System.getenv("DATABASE_PASSWORD"));
+		System.setProperty("SERVER.PORT", System.getenv("SERVER.PORT"));
 
-		System.setProperty("CLOUDINARY_API_KEY", System.get("CLOUDINARY_API_KEY"));
-		System.setProperty("CLOUDINARY_API_SECRET", System.get("CLOUDINARY_API_SECRET"));
-		System.setProperty("CLOUDINARY_NAME", System.get("CLOUDINARY_NAME"));
+		System.setProperty("CLOUDINARY_API_KEY", System.getenv("CLOUDINARY_API_KEY"));
+		System.setProperty("CLOUDINARY_API_SECRET", System.getenv("CLOUDINARY_API_SECRET"));
+		System.setProperty("CLOUDINARY_NAME", System.getenv("CLOUDINARY_NAME"));
 
-		System.setProperty("MAIL_HOST", System.get("MAIL_HOST"));
-		System.setProperty("MAIL_PASSWORD", System.get("MAIL_PASSWORD"));
-		System.setProperty("MAIL_PORT", System.get("MAIL_PORT"));
-		System.setProperty("MAIL_USERNAME", System.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_HOST", System.getenv("MAIL_HOST"));
+		System.setProperty("MAIL_PASSWORD", System.getenv("MAIL_PASSWORD"));
+		System.setProperty("MAIL_PORT", System.getenv("MAIL_PORT"));
+		System.setProperty("MAIL_USERNAME", System.getenv("MAIL_USERNAME"));
 
-		System.setProperty("GOOGLE_AUTHORIZATION_URI", System.get("GOOGLE_AUTHORIZATION_URI"));
-		System.setProperty("GOOGLE_TOKEN_URI", System.get("GOOGLE_TOKEN_URI"));
-		System.setProperty("GOOGLE_USER_INFO_URI", System.get("GOOGLE_USER_INFO_URI"));
-		System.setProperty("GOOGLE_CLIENT_ID", System.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", System.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("GOOGLE_REDIRECT_URI", System.get("GOOGLE_REDIRECT_URI"));
-		System.setProperty("GOOGLE_SCOPE", System.get("GOOGLE_SCOPE"));
+		System.setProperty("GOOGLE_AUTHORIZATION_URI", System.getenv("GOOGLE_AUTHORIZATION_URI"));
+		System.setProperty("GOOGLE_TOKEN_URI", System.getenv("GOOGLE_TOKEN_URI"));
+		System.setProperty("GOOGLE_USER_INFO_URI", System.getenv("GOOGLE_USER_INFO_URI"));
+		System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("GOOGLE_REDIRECT_URI", System.getenv("GOOGLE_REDIRECT_URI"));
+		System.setProperty("GOOGLE_SCOPE", System.getenv("GOOGLE_SCOPE"));
 
-		System.setProperty("PAYPAL_CLIENT_ID", System.get("PAYPAL_CLIENT_ID"));
-		System.setProperty("PAYPAL_CLIENT_SECRET", System.get("PAYPAL_CLIENT_SECRET"));
-		System.setProperty("PAYPAL_MODE", System.get("PAYPAL_MODE"));
+		System.setProperty("PAYPAL_CLIENT_ID", System.getenv("PAYPAL_CLIENT_ID"));
+		System.setProperty("PAYPAL_CLIENT_SECRET", System.getenv("PAYPAL_CLIENT_SECRET"));
+		System.setProperty("PAYPAL_MODE", System.getenv("PAYPAL_MODE"));
 
-		System.setProperty("VNP_TMNCODE", System.get("VNP_TMNCODE"));
-		System.setProperty("VNP_HASHSECRET", System.get("VNP_HASHSECRET"));
-		System.setProperty("VNP_URL", System.get("VNP_URL"));
-		System.setProperty("VNP_RETURN_URL", System.get("VNP_RETURN_URL"));
+		System.setProperty("VNP_TMNCODE", System.getenv("VNP_TMNCODE"));
+		System.setProperty("VNP_HASHSECRET", System.getenv("VNP_HASHSECRET"));
+		System.setProperty("VNP_URL", System.getenv("VNP_URL"));
+		System.setProperty("VNP_RETURN_URL", System.getenv("VNP_RETURN_URL"));
 
 		SpringApplication.run(BarbershopBackendApplication.class, args);
 	}
